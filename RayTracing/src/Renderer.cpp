20e5edbb190b11f202cf0a9c60508eb8bcd7cc65
Nam <lkunelk@gmail.com>
2023-08-17 09:45:53 -0700

    render red, green gradient

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -16,17 +16,27 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 	}
 
-	delete[] m_ImageData;
+	delete[] m_ImageData; // delete null object is ok
 	m_ImageData = new uint32_t[width * height];
 }
 
 void Renderer::Render()
 {
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
+	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) // start with y on outside to be friendly to cache, makes things contiguous?
 	{
-		m_ImageData[i] = Walnut::Random::UInt(); //  ABGR format
-		m_ImageData[i] |= 0xff000000; // set alpha to ff
+		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++)
+		{
+			glm::vec2 coord = {(float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight()};
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord); // pixel format ABGR
+		}
 	}
-
 	m_FinalImage->SetData(m_ImageData); // send to GPU
+}
+
+uint32_t Renderer::PerPixel(glm::vec2 coord)
+{
+	uint8_t red = (uint8_t)(coord.x * 255);
+	uint8_t green = (uint8_t)(coord.y * 255);
+
+	return 0xff000000 | (green << 8) | red;
 }
